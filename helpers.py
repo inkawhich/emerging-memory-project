691d9def099599d3f61bdcc3364eae6b33fe732b
Xiaoxuan-Yang <yangxx94@hotmail.com>
2019-03-30 01:25:52 -0400

    update training

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import attack
+import quantilization
 # class AverageMeter(object):
 # 	"""Computes and stores the average and current value"""
 # 	def __init__(self):
@@ -59,9 +60,9 @@ def test_model_AT(model,device,loader):
 def quan_param(model,nbits,do_linear):
     for param in model.parameters():
         if do_linear:
-            param.data=quantilization.Quantilizer(nbits).apply(param.data)
+            param.data=quantilization.Quantizer(nbits).apply(param.data,nbits)
         else:
-            param.data=quantilization.Quantilizer_nonlinear(nbits).apply(param.data)
+            param.data=quantilization.Quantizer_nonlinear(nbits).apply(param.data,nbits)
 def test_model_quan(model,device,loader,nbits,do_linear):
     quan_param(model,nbits,do_linear)
     model.eval()
@@ -82,15 +83,15 @@ def test_model_quan_AT(model,device,loader,nbits,do_linear):
     quan_param(model,nbits,do_linear)
     model.eval()
     acc_sum = 0.;loss_sum = 0.;cnt = 0.
-    with torch.no_grad():
-        for dat,lbl in loader:
-            dat,lbl = dat.to(device),lbl.to(device)
-            dat = attack.PGD_attack_test(model, device, dat, lbl, eps=AT_eps, alpha=AT_alpha, iters=AT_iters)
-            output = model(dat)
-            loss_sum += F.cross_entropy(output,lbl).item()
-            pred = output.argmax(dim=1, keepdim=True)
-            correct = pred.eq(lbl.view_as(pred)).sum().item()
-            acc_sum += correct
-            cnt += lbl.size(0)
+    #with torch.no_grad():
+    for dat,lbl in loader:
+        dat,lbl = dat.to(device),lbl.to(device)
+        dat = attack.PGD_attack(model, device, dat, lbl, eps=AT_eps, alpha=AT_alpha, iters=AT_iters)
+        output = model(dat)
+        loss_sum += F.cross_entropy(output,lbl).item()
+        pred = output.argmax(dim=1, keepdim=True)
+        correct = pred.eq(lbl.view_as(pred)).sum().item()
+        acc_sum += correct
+        cnt += lbl.size(0)
     model.train()
     return acc_sum/cnt,loss_sum/cnt
